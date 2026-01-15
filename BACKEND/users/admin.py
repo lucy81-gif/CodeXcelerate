9cfa3c1ececf5407d4cb7f49522672cafb336853
Thato Motseki <thatomotseki292@gmail.com>
2026-01-15 11:41:24 +0200

    1.Profile Model 2.Django Admin setup

--- BACKEND/users/admin.py
+++ BACKEND/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin (admin.ModelAdmin):
+    list_display = ('user','xp', 'streak', 'created_at')
+    search_fields = ('user__username',)
