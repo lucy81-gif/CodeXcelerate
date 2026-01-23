d2096f17c62b45295f48d244728af9fe000dbaa8
Thato Motseki <thatomotseki292@gmail.com>
2026-01-23 14:48:15 +0200

    Register endpoint: serializer

--- BACKEND/users/serializers.py
+++ BACKEND/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from django.contrib.auth.models import User
 
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
@@ -24,3 +25,25 @@ class LeaderboardSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ['rank','username','xp','streak']
         
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only = True)
+    password2 = serializers.CharField(write_only = True)
+
+    class Meta:
+        model = User
+        fields =('username', 'email', 'password','password2')
+
+        def validate (self, attrs):
+            if attrs['password']!=attrs['password2']:
+                raise serializers.ValidationError("Passwords do not match")
+            return attrs
+        
+        def create(self, validated_data):
+            validated_data.pop('password2')
+
+            user = User.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data.get('email'),
+                password=validated_data['password']
+            )
+            return user
